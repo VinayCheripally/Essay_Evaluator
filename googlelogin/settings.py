337fb93e382de8c439cd6528bce76dd8de948d09
VinayCheripally <vinaycheripally1@gmail.com>
2024-04-20 16:31:25 +0530

    ad

--- googlelogin/settings.py
+++ googlelogin/settings.py
@@ -93,10 +93,6 @@ WSGI_APPLICATION = 'googlelogin.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
