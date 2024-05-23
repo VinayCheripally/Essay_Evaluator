8be69e5a86291fc481301e1d17721f6d208f4048
VinayCheripally <vinaycheripally1@gmail.com>
2024-05-23 10:39:55 +0530

    add

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Upgrade pip, setuptools, and wheel
-pip install --upgrade pip setuptools wheel
+# Upgrade pip, then install specific versions of setuptools and wheel
+pip install --upgrade pip
+pip install setuptools==57.5.0 wheel==0.36.2
 
 # Install the dependencies from requirements.txt
 pip install --upgrade -r /vercel/path0/requirements.txt
