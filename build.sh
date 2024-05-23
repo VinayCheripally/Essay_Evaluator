322c37652e4155a539b01e212ffd8c8c7857d534
VinayCheripally <vinaycheripally1@gmail.com>
2024-05-23 23:13:46 +0530

    add

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Exit immediately if a command exits with a non-zero status
+# set -e
+
+# Upgrade pip, then install specific versions of setuptools and wheel
+pip install --upgrade pip
+pip install setuptools==57.5.0 wheel==0.36.2
+
+# Install the dependencies from requirements.txt
+#!/bin/bash
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
