8818b4915692f2d2b95b08da21ff641da9d87c8c
VinayCheripally <vinaycheripally1@gmail.com>
2024-05-23 22:59:27 +0530

    add

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # set -e
 
 # Upgrade pip, then install specific versions of setuptools and wheel
-# pip install --upgrade pip
-# pip install setuptools==57.5.0 wheel==0.36.2
+pip install --upgrade pip
+pip install setuptools==57.5.0 wheel==0.36.2
 
 # Install the dependencies from requirements.txt
 pip install -r requirements.txt
