17e63d8b0e90bc5fecfab444e62af24bb319a192
VinayCheripally <vinaycheripally1@gmail.com>
2024-05-23 22:56:09 +0530

    add

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
-set -e
+# set -e
 
 # Upgrade pip, then install specific versions of setuptools and wheel
-pip install --upgrade pip
-pip install setuptools==57.5.0 wheel==0.36.2
+# pip install --upgrade pip
+# pip install setuptools==57.5.0 wheel==0.36.2
 
 # Install the dependencies from requirements.txt
-pip install --upgrade -r /vercel/path0/requirements.txt
+pip install -r requirements.txt
 
 # Run Django migrations
-python manage.py migrate
+# python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
